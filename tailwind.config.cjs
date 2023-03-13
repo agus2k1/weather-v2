57a284f73b59611e0a7b699ffe31e49a6f3bdea3
West <agus2kcoin@outlook.com>
2023-03-12 23:53:20 -0300

    Add background image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        rain: "url('/bk-images/rain-background-2.jpg')",
+      },
+    },
   },
   plugins: [],
 };
