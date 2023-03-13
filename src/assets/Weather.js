8f1c8079507430d4499eec357d8398db234fac0e
West <agus2kcoin@outlook.com>
2023-03-12 23:52:51 -0300

    Feature temporarly api data

---
+++ src/assets/Weather.js
@@ -0,0 +1,29 @@
+const weatherObject = {
+  latitude: -34.625,
+  longitude: -58.5,
+  generationtime_ms: 0.23603439331054688,
+  utc_offset_seconds: 0,
+  timezone: 'GMT',
+  timezone_abbreviation: 'GMT',
+  elevation: 37,
+  daily_units: {
+    time: 'iso8601',
+    temperature_2m_max: '°C',
+    temperature_2m_min: '°C',
+  },
+  daily: {
+    time: [
+      '2023-03-11',
+      '2023-03-12',
+      '2023-03-13',
+      '2023-03-14',
+      '2023-03-15',
+      '2023-03-16',
+      '2023-03-17',
+    ],
+    temperature_2m_max: [37.7, 35.8, 35, 36.5, 29.7, 28.2, 28],
+    temperature_2m_min: [26.8, 27.5, 26.4, 26.9, 26.1, 24.4, 24],
+  },
+};
+
+export default weatherObject;
