827410ae4110a5fa0775f1d16a45b89a8426842d
West <agus2kcoin@outlook.com>
2023-03-13 23:26:16 -0300

    Style option

--- src/Components/Functionality/Option.jsx
+++ src/Components/Functionality/Option.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Option = ({ name, logo, color }) => {
   return (
     <span
-      className={`w-48 flex items-center justify-center gap-1 border-b-4 border-b-${color}-500 cursor-pointer`}
+      className="w-48 flex items-center justify-center gap-1 border-b-4 cursor-pointer"
+      style={{ borderColor: `${color}` }}
     >
       <img src={`/design/line/export/${logo}.svg`} alt={name} className="w-8" />
       <p className="h-5">{name}</p>
