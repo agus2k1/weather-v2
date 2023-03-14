3996f73882153d2a3051a22f79a1c13b39e266fc
West <agus2kcoin@outlook.com>
2023-03-13 23:21:46 -0300

    Add tab animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {
       backgroundImage: {
         rain: "url('/bk-images/rain-background-2.jpg')",
+      },
+      animation: {
+        openTab: 'openTab 1s ease-in-out forwards',
+        closeTab: 'closeTab 1s ease-in-out forwards',
+      },
+      keyframes: {
+        openTab: {
+          '0%': { height: '0', opacity: '0' },
+          '100%': { height: '13rem', opacity: '1' },
+        },
+        closeTab: {
+          '0%': { height: '13rem', opacity: '1' },
+          '100%': { height: '0', opacity: '0' },
+        },
       },
     },
   },
