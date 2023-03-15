7ecca97867348a7b1a7c6ec22d429b8584fcba9a
West <agus2kcoin@outlook.com>
2023-03-14 23:53:36 -0300

    Add context provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { WeatherProvider } from './context/WeatherContext';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <WeatherProvider>
+      <App />
+    </WeatherProvider>
   </React.StrictMode>
 );
