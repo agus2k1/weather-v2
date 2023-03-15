efc787c10ffdac16d13577b7111169de6767184e
West <agus2kcoin@outlook.com>
2023-03-14 23:54:05 -0300

    Add context props

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import Search from './Components/Functionality/Search';
-import Option from './Components/Functionality/Option';
+import FooterTabs from './Components/Functionality/FooterTabs';
 import ActualWeather from './Components/UI/ActualWeather';
+import WeeklyWeather from './Components/UI/WeeklyWeather';
 import DailyWeather from './Components/UI/DailyWeather';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import Lights from './Components/Lights';
+import Lights from './Components/Canvas/Lights';
+import { useWeatherContext } from './context/WeatherContext';
 
 const App = () => {
+  const { cityName } = useWeatherContext();
   const [start, setStart] = useState(true);
   const [openTab, setOpenTab] = useState(null);
   // const aspectRatio = window.innerWidth / window.innerHeight;
@@ -24,30 +27,20 @@ const App = () => {
   return (
     <div className="w-screen h-screen bg-cover bg-center bg-rain relative overflow-hidden">
       {/* Main */}
-      <section className="w-full h-full py-20 px-28 flex flex-col justify-between">
-        <span className="flex items-end gap-5">
-          <img src="/location-logo.png" alt="location" />
-          <p className="h-12 text-5xl font-extralight">London, UK</p>
+      <section className="w-full h-full py-20 px-28 flex justify-between">
+        <span className="flex items-start gap-5">
+          <img src="/location-logo.png" alt="location" className="w-12" />
+          <p className="h-12 text-5xl font-extralight">
+            {cityName &&
+              cityName[0].toUpperCase() + cityName.slice(1, cityName.length)}
+          </p>
         </span>
+        <Search />
       </section>
       {/* Menu options */}
       <div className="w-full flex flex-col absolute bottom-0">
         <div className="flex" onClick={() => handleTabClick()}>
-          <Option
-            name={'Weather'}
-            logo={'wi_partly-cloudy-day'}
-            color={'#f59e0b'}
-          />
-          <Option
-            name={'Weather'}
-            logo={'wi_partly-cloudy-day'}
-            color={'#6366f1'}
-          />
-          <Option
-            name={'Weather'}
-            logo={'wi_partly-cloudy-day'}
-            color={'#10b981'}
-          />
+          <FooterTabs />
         </div>
         {!start && (
           <div
@@ -56,8 +49,8 @@ const App = () => {
               openTab ? 'animate-openTab' : 'animate-closeTab'
             }`}
           >
-            <div className="h-full w-full flex">
-              <div className="pr-14">
+            <div className="h-full w-full flex justify-between">
+              <div className=" pr-14">
                 <ActualWeather
                   temp={'28º'}
                   day={'MONDAY 27th'}
@@ -65,17 +58,13 @@ const App = () => {
                   minTemp={'67º'}
                 />
               </div>
-              <DailyWeather day={'TUE'} temp={'21º'} />
-              <DailyWeather day={'WED'} temp={'21º'} />
-              <DailyWeather day={'THU'} temp={'21º'} />
-              <DailyWeather day={'FRI'} temp={'21º'} />
-              <DailyWeather day={'SAT'} temp={'21º'} />
-              <DailyWeather day={'SUN'} temp={'21º'} />
+              <div className=" flex">
+                <WeeklyWeather />
+              </div>
             </div>
           </div>
         )}
       </div>
-
       {/* <Canvas frameloop="demand" className="bg-blue-300">
         <Suspense fallback={null}></Suspense>
         <OrbitControls />
