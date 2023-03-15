53885a33661b25127b81c5bbb4b9642d1577a158
West <agus2kcoin@outlook.com>
2023-03-14 23:48:51 -0300

    Feature footer tabs

---
+++ src/Components/Functionality/FooterTabs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tab from './Tab';
+
+function FooterTabs() {
+  return (
+    <>
+      <Tab name={'Weather'} logo={'wi_partly-cloudy-day'} color={'#f59e0b'} />
+      <Tab name={'Weather'} logo={'wi_partly-cloudy-day'} color={'#6366f1'} />
+      <Tab name={'Weather'} logo={'wi_partly-cloudy-day'} color={'#10b981'} />
+    </>
+  );
+}
+
+export default FooterTabs;
