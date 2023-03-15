a5db765dcf81aea1a3fc58c6be2b59935d1f3361
West <agus2kcoin@outlook.com>
2023-03-14 23:55:00 -0300

    Add context props

--- src/Components/UI/ActualWeather.jsx
+++ src/Components/UI/ActualWeather.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useWeatherContext } from '../../context/WeatherContext';
 
-const ActualWeather = ({ temp, day, wind, minTemp }) => {
+const ActualWeather = ({ temp, wind, minTemp }) => {
+  const { day } = useWeatherContext();
+
   return (
     <>
-      <div className="flex items-center mt-3">
+      <div className="flex items-center justify-between mt-3">
         <p className="text-8xl font-semibold pt-5 pl-1">{temp}</p>
         <img
           src="/design/fill/animation-ready/rain.svg"
@@ -11,7 +14,7 @@ const ActualWeather = ({ temp, day, wind, minTemp }) => {
           className="w-32"
         />
       </div>
-      <div className="flex items-center gap-10">
+      <div className="flex items-center justify-between ">
         <p className="font-light bg-zinc-600 px-2 pt-1 rounded-2xl">{day}</p>
         <p className="text-1xl font-bold">
           {wind} <span className="text-slate-400">/</span> {minTemp}
