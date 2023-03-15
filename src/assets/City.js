f56fd20a1a8794dc7eae587988bedc4d3204a500
West <agus2kcoin@outlook.com>
2023-03-14 23:50:58 -0300

    Add temporarly city data

---
+++ src/assets/City.js
@@ -0,0 +1,18 @@
+const cityObject = {
+  place_id: 298239241,
+  licence:
+    'Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright',
+  osm_type: 'relation',
+  osm_id: 1224652,
+  boundingbox: ['-34.705637', '-34.5265535', '-58.5314494', '-58.3351423'],
+  lat: '-34.6075682',
+  lon: '-58.4370894',
+  display_name:
+    'Buenos Aires, Comuna 6, Ciudad Autónoma de Buenos Aires, Argentina',
+  class: 'boundary',
+  type: 'administrative',
+  importance: 0.7279817834797908,
+  icon: 'https://nominatim.openstreetmap.org/ui/mapicons/poi_boundary_administrative.p.20.png',
+};
+
+export default cityObject;
