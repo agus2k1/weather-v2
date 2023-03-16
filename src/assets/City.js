162a057f23fb2bdf950cc7b9a30608c6505d4d49
West <agus2kcoin@outlook.com>
2023-03-15 22:52:39 -0300

    Update temporarly city data

--- src/assets/City.js
+++ src/assets/City.js
@@ -5,8 +5,8 @@ const cityObject = {
   osm_type: 'relation',
   osm_id: 1224652,
   boundingbox: ['-34.705637', '-34.5265535', '-58.5314494', '-58.3351423'],
-  lat: '-34.6075682',
-  lon: '-58.4370894',
+  lat: '-34.6',
+  lon: '-58.4',
   display_name:
     'Buenos Aires, Comuna 6, Ciudad Autónoma de Buenos Aires, Argentina',
   class: 'boundary',
