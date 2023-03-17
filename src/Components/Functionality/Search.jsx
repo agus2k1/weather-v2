6afe0fc859cd379c310be4f9fe552f8dba344d1a
West <agus2kcoin@outlook.com>
2023-03-16 23:21:11 -0300

    Add search filter

--- src/Components/Functionality/Search.jsx
+++ src/Components/Functionality/Search.jsx
@@ -1,35 +1,87 @@
 import React, { useState, useRef } from 'react';
+import cities from '../../assets/cities';
 import { useWeatherContext } from '../../context/WeatherContext';
 
 const Search = () => {
-  const { setCityName } = useWeatherContext();
+  const { setWeatherData, setCityName } = useWeatherContext();
   const [searchTerm, setSearchTerm] = useState('');
+  const [filteredCities, setFilteredCities] = useState([]);
 
   const searchValue = useRef();
 
-  const handleOnSubmit = (e) => {
+  const handleOnSubmit = (e, cityName) => {
     e.preventDefault();
-    setCityName(searchTerm.toLowerCase());
-    console.log('submit');
+    setWeatherData('');
+    if (cityName) {
+      setFilteredCities([]);
+      searchValue.current.value = '';
+      setCityName(cityName.toLowerCase());
+    } else if (
+      filteredCities.some(
+        (city) => city.name.toLowerCase() === searchTerm.toLowerCase()
+      )
+    ) {
+      setFilteredCities([]);
+      searchValue.current.value = '';
+      setCityName(searchTerm.toLowerCase());
+    }
   };
 
   const handleOnSearch = () => {
-    setSearchTerm(searchValue.current.value);
+    const actualSearch = searchValue.current.value;
+    setSearchTerm(actualSearch);
+    console.log(actualSearch);
+
+    const filter = cities.filter((city) =>
+      city.name.toLowerCase().includes(actualSearch.toLowerCase())
+    );
+
+    if (actualSearch.length > 2) {
+      const citiesNames = filter.map((city) => city.name);
+
+      const uniqueArray = filter.filter(
+        ({ name }, index) => !citiesNames.includes(name, index + 1)
+      );
+
+      setFilteredCities(uniqueArray);
+    } else {
+      actualSearch === '' ? setFilteredCities([]) : setFilteredCities(filter);
+    }
   };
 
   return (
-    <form className="w-50 h-50" onSubmit={(e) => handleOnSubmit(e)}>
-      <div className="p-4">
-        <label htmlFor="name" className="mr-2">
-          Search for city
-        </label>
+    <form className="w-56 h-52" onSubmit={(e) => handleOnSubmit(e)}>
+      <div className="w-full mt-4 py-1 px-2 flex items-center bg-white rounded-lg border-none">
         <input
           type="text"
           id="name"
           ref={searchValue}
+          placeholder="Search for city..."
           onChange={handleOnSearch}
+          className="w-44 h-8 text-base bg-white text-black pt-1 pl-2 focus:outline-none"
         />
+        <span className="cursor-pointer" onClick={(e) => handleOnSubmit(e)}>
+          <img src="/search-logo.png" alt="search" />
+        </span>
       </div>
+      {filteredCities.length !== 0 && (
+        <div
+          className="w-44 h-32 bg-white shadow-lg overflow-hidden overflow-y-auto rounded-lg mt-1 scrollbar-hide"
+          style={{}}
+        >
+          {filteredCities.slice(0, 10).map((city) => {
+            return (
+              <p
+                key={city.name}
+                className="w-full h-8 text-base flex text-black pt-2 pl-2 hover:bg-gray-300 cursor-pointer overflow-hidden"
+                onClick={(e) => handleOnSubmit(e, city.name)}
+              >
+                {city.name}
+              </p>
+            );
+          })}
+        </div>
+      )}
     </form>
   );
 };
