dd428f90a3b453e2586f65a50d0bed5ff53fad6b
West <agus2kcoin@outlook.com>
2023-03-16 23:15:30 -0300

    Feature loader

---
+++ src/Components/UI/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <img
+      src="/design/fill/animation-ready/tornado.svg"
+      alt="loading..."
+      className="h-full"
+    />
+  );
+};
+
+export default Loader;
