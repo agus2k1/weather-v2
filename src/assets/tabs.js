4ce2e656ac1af23800ae10dc40565196809b9823
West <agus2kcoin@outlook.com>
2023-03-17 17:22:45 -0300

    Add tabs

---
+++ src/assets/tabs.js
@@ -0,0 +1,11 @@
+const tabs = [
+  {
+    name: 'Weather',
+    logo: 'wi_partly-cloudy-day',
+    color: '#f59e0b',
+  },
+  { name: 'Icons', logo: 'wi_umbrella', color: '#6366f1' },
+  // { name: 'Weather', logo: 'wi_partly-cloudy-day', color: '#10b981' },
+];
+
+export default tabs;
