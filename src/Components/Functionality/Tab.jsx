0edab5de0aaab7d2b1dbdb3d79c85dd235aa5279
West <agus2kcoin@outlook.com>
2023-03-17 17:26:14 -0300

    Add tab click functionality

--- src/Components/Functionality/Tab.jsx
+++ src/Components/Functionality/Tab.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import { useWeatherContext } from '../../context/WeatherContext';
 
-const Tab = ({ name, logo, color }) => {
+const Tab = ({ index, name, logo, color }) => {
+  const {
+    start,
+    openTab,
+    activeTabIndex,
+    setActiveTabIndex,
+    setStart,
+    setOpenTab,
+    cityName,
+  } = useWeatherContext();
+
+  const handleTabClick = () => {
+    if (cityName) {
+      if (start) {
+        setStart(false);
+      }
+      if (openTab && activeTabIndex === index) {
+        setOpenTab(false);
+      } else {
+        setOpenTab(true);
+      }
+      setActiveTabIndex(index);
+    }
+  };
+
   return (
-    <span
+    <div
       className="w-48 flex items-center justify-center gap-1 border-b-4 cursor-pointer"
       style={{ borderColor: `${color}` }}
+      onClick={() => handleTabClick()}
     >
       <img src={`/design/line/export/${logo}.svg`} alt={name} className="w-8" />
       <p className="h-5">{name}</p>
-    </span>
+    </div>
   );
 };
 
