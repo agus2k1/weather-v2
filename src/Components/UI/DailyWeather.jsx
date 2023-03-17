8b713e876416eaddeef70ab4e95d60ee31e9c6fa
West <agus2kcoin@outlook.com>
2023-03-17 17:27:58 -0300

    Style media queries

--- src/Components/UI/DailyWeather.jsx
+++ src/Components/UI/DailyWeather.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
-const DailyWeather = ({ day, logo, temp }) => {
+const DailyWeather = ({ day, logo, temp, active }) => {
   return (
-    <div className="h-full border-l-2 border-slate-500 flex flex-col items-center">
+    <div
+      className={`h-full border-l-2 border-slate-500 flex flex-col items-center sm:flex ${
+        !active && 'hidden'
+      }`}
+    >
       <span className="font-light bg-zinc-600 px-2 pt-1 rounded-2xl mt-2 lg:mt-1 md:mt-3 sm:mt-9">
         {day}
       </span>
