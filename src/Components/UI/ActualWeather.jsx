af9d720d056b018cd1b81d3c9a0457e79fcfc217
West <agus2kcoin@outlook.com>
2023-03-16 23:22:48 -0300

    Style media queries

--- src/Components/UI/ActualWeather.jsx
+++ src/Components/UI/ActualWeather.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useWeatherContext } from '../../context/WeatherContext';
 
 const ActualWeather = ({ wind, minTemp }) => {
@@ -9,22 +9,24 @@ const ActualWeather = ({ wind, minTemp }) => {
   const logo = getWeatherLogo(weatherData?.current_weather.weathercode, false);
 
   return (
-    <>
-      <div className="flex items-center justify-between mt-3">
-        <p className="text-8xl font-semibold pt-5 pl-1">{temp + 'º'}</p>
+    <div className="w-auto pl-10 pr-6 flex gap-4 xxs:pl-6 xs:pl-10">
+      <div className="flex flex-col text-center mt-10 xxs:mt-14 xs:mt-10">
+        <p className="text-8xl font-semibold xxs:text-6xl xs:text-8xl">
+          {temp + 'º'}
+        </p>
+        <p className="font-light bg-zinc-600 px-2 pt-1 rounded-2xl">{day}</p>
+      </div>
+      <div className="w-32 flex flex-col text-center mt-3 xxs:w-20 xxs:mt-8 xs:w-32 xs:mt-3">
         <img
           src={`/design/fill/animation-ready/${logo}.svg`}
           alt={logo}
-          className="w-32"
+          className="w-32 xxs:w-20 xs:w-32"
         />
-      </div>
-      <div className="flex items-center justify-between ">
-        <p className="font-light bg-zinc-600 px-2 pt-1 rounded-2xl">{day}</p>
         <p className="text-1xl font-bold">
           {wind} <span className="text-slate-400">/</span> {minTemp}
         </p>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/Components/UI/DailyWeather.jsx
+++ src/Components/UI/DailyWeather.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import { useWeatherContext } from '../../context/WeatherContext';
 
 const DailyWeather = ({ day, logo, temp }) => {
   return (
-    <div className="border-l-2 border-slate-500">
-      <div className="h-full flex flex-col items-center mt-5 px-6">
-        <span className="font-light bg-zinc-600 px-2 pt-1 rounded-2xl">
-          {day}
-        </span>
-        <img
-          src={`/design/fill/animation-ready/${logo}.svg`}
-          alt={logo}
-          className="w-28"
-        />
-        <p className="text-3xl font-bold">{temp}</p>
-      </div>
+    <div className="h-full border-l-2 border-slate-500 flex flex-col items-center">
+      <span className="font-light bg-zinc-600 px-2 pt-1 rounded-2xl mt-2 lg:mt-1 md:mt-3 sm:mt-9">
+        {day}
+      </span>
+      <img
+        src={`/design/fill/animation-ready/${logo}.svg`}
+        alt={logo}
+        className="w-32 lg:w-36"
+      />
+      <p className="text-3xl font-bold">{temp}</p>
     </div>
   );
 };
